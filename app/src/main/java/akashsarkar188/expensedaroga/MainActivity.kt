2e5ab4c0e0be95bcfe1750d2ead14656201a08e2
Akash Sarkar <akash.sarkar188@gmail.com>
2022-10-03 01:37:21 +0530

    Initial commit

---
+++ app/src/main/java/akashsarkar188/expensedaroga/MainActivity.kt
@@ -0,0 +1,11 @@
+package akashsarkar188.expensedaroga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreat e(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
