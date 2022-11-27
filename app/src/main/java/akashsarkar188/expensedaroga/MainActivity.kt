2ffd432adf2790401a6173ea229b137f92ceeaf6
Akash Sarkar <akash.sarkar188@gmail.com>
2022-11-27 07:42:38 +0530

    [WIP] -- Added some fragment logics -- Created new bottom nav

--- app/src/main/java/akashsarkar188/expensedaroga/MainActivity.kt
+++ app/src/main/java/akashsarkar188/expensedaroga/MainActivity.kt
@@ -1,12 +1,21 @@
 package akashsarkar188.expensedaroga
 
 import akashsarkar188.expensedaroga.databinding.ActivityMainBinding
+import akashsarkar188.expensedaroga.databinding.BottomNavItemBinding
 import akashsarkar188.expensedaroga.screens.home.HomeFragment
+import akashsarkar188.expensedaroga.screens.settings.SettingsFragment
 import akashsarkar188.expensedaroga.services.FragmentHelper
 import akashsarkar188.expensedaroga.utils.NotificationHelper
+import akashsarkar188.expensedaroga.utils.commonMethods.doIfFalse
+import akashsarkar188.expensedaroga.utils.commonMethods.doIfTrue
 import akashsarkar188.expensedaroga.utils.commonMethods.getCurrentMonthYearString
+import android.animation.LayoutTransition
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 
@@ -25,11 +34,96 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        FragmentHelper.addFragment(HomeFragment.newInstance())
-
         binding?.openBubbleFAB?.setOnClickListener {
             NotificationHelper.openTransactionsInBubble(this, getCurrentMonthYearString())
         }
 
+        initBottomNavigation()
+    }
+
+    private val homeFragment : HomeFragment by lazy {
+        HomeFragment()
+    }
+    private val settingsFragment : SettingsFragment by lazy {
+        SettingsFragment()
+    }
+
+    private fun initBottomNavigation() {
+        binding?.homeLayout?.parentCard?.setOnClickListener {
+            changeBottomNavSelection(binding?.homeContainerLinearLayout, binding?.homeLayout, true)
+            changeBottomNavSelection(binding?.settingsContainerLinearLayout, binding?.settingsLayout, false)
+            if (FragmentHelper.isFragmentAdded(homeFragment)) {
+                FragmentHelper.showThisFragment(homeFragment)
+            } else {
+                FragmentHelper.addFragment(homeFragment, false)
+            }
+        }
+
+        binding?.settingsLayout?.parentCard?.setOnClickListener {
+            changeBottomNavSelection(binding?.homeContainerLinearLayout, binding?.homeLayout, false)
+            changeBottomNavSelection(binding?.settingsContainerLinearLayout, binding?.settingsLayout, true)
+            if (FragmentHelper.isFragmentAdded(settingsFragment)) {
+                FragmentHelper.showThisFragment(settingsFragment)
+            } else {
+                FragmentHelper.addFragment(settingsFragment, false)
+            }
+        }
+
+        setupLayout(binding?.homeLayout, "Home", R.drawable.home_icon)
+        setupLayout(binding?.settingsLayout, "Settings", R.drawable.setting_icon)
+
+        binding?.homeLayout?.parentCard?.performClick()
+    }
+
+    private fun setupLayout(binding: BottomNavItemBinding?, titleStr: String, iconId: Int) {
+        binding?.apply {
+            icon.setImageDrawable(ContextCompat.getDrawable(this@MainActivity, iconId))
+            title.text = titleStr
+        }
+    }
+
+    private fun changeBottomNavSelection(parentContainer: View?, binding: BottomNavItemBinding?, setSelected: Boolean) {
+        binding?.apply {
+            (parentContainer as ViewGroup).layoutTransition.enableTransitionType(
+                LayoutTransition.CHANGING
+            )
+            doIfTrue(setSelected) {
+                cardLinearLayout.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.themeGreenOpacity15
+                    )
+                )
+                icon.imageTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.themeGreenDarker
+                    )
+                )
+                title.setTextColor(ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.themeGreenDarker
+                    )
+                ))
+                title.visibility = View.VISIBLE
+            }
+
+            doIfFalse(setSelected) {
+                cardLinearLayout.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.white
+                    )
+                )
+                icon.imageTintList = ColorStateList.valueOf(
+                    ContextCompat.getColor(
+                        this@MainActivity,
+                        R.color.basicBlack
+                    )
+                )
+                title.visibility = View.GONE
+            }
+        }
     }
 }
--- app/src/main/java/akashsarkar188/expensedaroga/services/FragmentHelper.kt
+++ app/src/main/java/akashsarkar188/expensedaroga/services/FragmentHelper.kt
@@ -3,6 +3,7 @@ package akashsarkar188.expensedaroga.services
 import akashsarkar188.expensedaroga.R
 import akashsarkar188.expensedaroga.utils.commonMethods.doIfFalse
 import akashsarkar188.expensedaroga.utils.commonMethods.doIfTrue
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import java.lang.ref.WeakReference
@@ -10,12 +11,13 @@ import java.lang.ref.WeakReference
 object FragmentHelper {
 
     private var activityContext: WeakReference<AppCompatActivity?>? = null
+    private var lastActiveFragment: WeakReference<Fragment?>? = null
 
     fun setContext(activity: AppCompatActivity) {
         activityContext = WeakReference(activity)
     }
 
-    fun addFragment(fragment: Fragment, addToBackstack : Boolean = true) {
+    fun addFragment(fragment: Fragment, addToBackstack: Boolean = true) {
         activityContext?.get()?.let { activityContext ->
             doIfFalse(activityContext.isFinishing) {
                 val fragmentManager = activityContext.supportFragmentManager
@@ -58,5 +60,33 @@ object FragmentHelper {
                 fragmentTransaction.commitAllowingStateLoss()
             }
         }
+    }
+
+    fun showThisFragment(fragment: Fragment) {
+        activityContext?.get()?.let {
+            val fragmentManager = it.supportFragmentManager
+
+            if (lastActiveFragment?.get() == null) {
+                fragmentManager
+                    .beginTransaction()
+                    .show(fragment)
+                    .commitAllowingStateLoss()
+            } else {
+                fragmentManager
+                    .beginTransaction()
+                    .hide(lastActiveFragment?.get()!!)
+                    .show(fragment)
+                    .commitAllowingStateLoss()
+            }
+
+            lastActiveFragment = WeakReference(fragment)
+        }
+    }
+
+    fun isFragmentAdded(fragment: Fragment): Boolean {
+        activityContext?.get()?.let {
+            return it.supportFragmentManager.findFragmentByTag(fragment.javaClass.canonicalName) == fragment
+        }
+        return false
     }
 }
